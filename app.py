5e58f59ed9ec1730ed69093dcbdde31f2e8f9b55
arturr0 <119508496+arturr0@users.noreply.github.com>
2023-12-27 17:52:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
-import asyncio
-import websockets
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-connected = set()
-
-@socketio.on('message')
-def handle_message(message):
-    print('Received message:', message)
-    for conn in connected:
-        socketio.emit('message', f'Got a new MSG FOR YOU: {message}', room=conn)
-
-@socketio.on('connect')
-def handle_connect():
-    connected.add(request.sid)
-    print('Client connected:', request.sid)
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    connected.remove(request.sid)
-    print('Client disconnected:', request.sid)
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('message')
+def handle_message(msg):
+    print('Received message:', msg)
+    socketio.emit('message', msg)
+
 if __name__ == '__main__':
-    socketio.run(app, port=process.env.PORT, debug=True)
+    # Use eventlet or gevent as the production server
+    # For Heroku, it's common to use eventlet
+    import eventlet
+    eventlet.monkey_patch()
+
+    # Run the application with SocketIO
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
