ce6ebc31d1ba412f5ad90c4bf5e96e77f964af40
arturr0 <119508496+arturr0@users.noreply.github.com>
2023-12-27 17:46:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, port=5000, debug=True)
+    socketio.run(app, port=process.env.PORT, debug=True)
