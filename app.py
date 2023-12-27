01536dda7177219cfd6edf6b8ec6f5ff74ab5146
arturr0 <119508496+arturr0@users.noreply.github.com>
2023-12-27 18:34:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ if __name__ == '__main__':
     eventlet.monkey_patch()
 
     # Run the application with SocketIO
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    socketio.app.run(debug=True)
+    
